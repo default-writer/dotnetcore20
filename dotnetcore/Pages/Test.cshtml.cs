f2d9f7bbc64671cdf3dd75f908096962bcbe4013
Artur Mustafin <artur.mustafin@gmail.com>
2017-12-07 01:24:35 +0300

    files new home

---
+++ dotnetcore/Pages/Test.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnetcore20.Pages
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
